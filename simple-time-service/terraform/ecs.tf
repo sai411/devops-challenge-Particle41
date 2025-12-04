eb443a3a3959a50f64e12de93c1054c4de53b8ff
sai411 <66815779+sai411@users.noreply.github.com>
2025-12-04 23:16:00 +0530

    Update ecs.tf

--- simple-time-service/terraform/ecs.tf
+++ simple-time-service/terraform/ecs.tf
@@ -57,6 +57,7 @@ resource "aws_lb_target_group" "tg" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  target_type = "ip"
   health_check {
     path                = "/"
     protocol            = "HTTP"
