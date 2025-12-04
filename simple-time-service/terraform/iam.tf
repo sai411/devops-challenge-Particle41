1351302e33edf50ca9d3807261afd9fedc80d85c
sai411 <66815779+sai411@users.noreply.github.com>
2025-12-04 21:05:53 +0530

    Update iam.tf

--- simple-time-service/terraform/iam.tf
+++ simple-time-service/terraform/iam.tf
@@ -1,1 +1,20 @@
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "${var.name_prefix}-ecs-task-exec"
 
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+}
+
+data "aws_iam_policy_document" "ecs_task_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
